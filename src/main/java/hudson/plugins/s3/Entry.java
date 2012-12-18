a15bc523008184406cfd72f3fbafb359f0910865
Jason Zerbe <jzerbe@vraidsys.com>
2012-12-18 10:39:50 -0600

    added S3 storage class selection at a per project granularity will only modify/set x-amz-storage-class HTTP header field if set in plugin configuration

--- src/main/java/hudson/plugins/s3/Entry.java
+++ src/main/java/hudson/plugins/s3/Entry.java
@@ -2,13 +2,20 @@ package hudson.plugins.s3;
 
 public final class Entry {
     /**
-     * Destination bucket for the copy. Can contain macros. 
+     * Destination bucket for the copy. Can contain macros.
      */
     public String bucket;
     /**
      * File name relative to the workspace root to upload.
      * Can contain macros and wildcards.
-     * <p>
      */
     public String sourceFile;
+    /**
+     * options for x-amz-storage-class can be STANDARD or REDUCED_REDUNDANCY
+     */
+    public static final String[] storageClasses = {"STANDARD", "REDUCED_REDUNDANCY"};
+    /**
+     * what x-amz-storage-class is currently set
+     */
+    public String storageClass;
 }
