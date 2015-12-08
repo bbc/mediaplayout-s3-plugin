77877fb806cbf0659120fce71c337c93c40a4294
Alexander <mr.akbashev@gmail.com>
2015-12-08 09:15:37 +0100

    Share transferManager instance between threads

--- src/main/java/hudson/plugins/s3/callable/AbstractS3Callable.java
+++ src/main/java/hudson/plugins/s3/callable/AbstractS3Callable.java
@@ -1,5 +1,6 @@
 package hudson.plugins.s3.callable;
 
+import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.transfer.TransferManager;
 import hudson.plugins.s3.ClientHelper;
@@ -14,8 +15,10 @@ public class AbstractS3Callable implements Serializable
     private final String accessKey;
     private final Secret secretKey;
     private final boolean useRole;
-    private transient AmazonS3Client client;
-    private transient TransferManager transferManager;
+
+    private transient static volatile AmazonS3Client client;
+    private transient static volatile TransferManager transferManager;
+    private transient static String oldClient;
 
     public AbstractS3Callable(String accessKey, Secret secretKey, boolean useRole)
     {
@@ -24,18 +27,43 @@ public class AbstractS3Callable implements Serializable
         this.useRole = useRole;
     }
 
-    protected AmazonS3Client getClient()
+    protected synchronized AmazonS3Client getClient()
     {
-        if (client == null) {
+        String newClient = getHash(accessKey, secretKey, useRole);
+
+        if (client == null || !newClient.equals(oldClient)) {
             client = ClientHelper.createClient(accessKey, secretKey, useRole);
+            oldClient = newClient;
         }
+
         return client;
     }
 
-    protected TransferManager getTransferManager()
+    private String getHash(String access, Secret secret, boolean useRole) {
+        return access + secret.getPlainText() + Boolean.toString(useRole);
+    }
+
+    protected synchronized TransferManager getTransferManager()
     {
-        if (transferManager == null)
+        if (transferManager == null) {
             transferManager = new TransferManager(getClient());
+        }
+        else {
+            AmazonS3 oldClient = transferManager.getAmazonS3Client();
+            AmazonS3 newClient = getClient();
+            if (!newClient.equals(oldClient)) {
+                while (transferManager != null) {
+                    try {
+                        transferManager.wait();
+                        transferManager.shutdownNow();
+                        transferManager = null;
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+            transferManager = new TransferManager(getClient());
+            }
+        }
 
         return transferManager;
     }
