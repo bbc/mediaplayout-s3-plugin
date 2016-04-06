ef6bc7a2ca76b4432ce0ffd71a3822dd8f59167a
Akbashev Alexander <mr.akbashev@gmail.com>
2016-04-06 14:09:49 +0200

    Fix credentials
    * Fix credentials
    
    * Fix compilation

--- src/main/java/hudson/plugins/s3/callable/S3DownloadCallable.java
+++ src/main/java/hudson/plugins/s3/callable/S3DownloadCallable.java
@@ -2,6 +2,7 @@ package hudson.plugins.s3.callable;
 
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.transfer.Download;
+import hudson.ProxyConfiguration;
 import hudson.plugins.s3.Destination;
 import hudson.plugins.s3.FingerprintRecord;
 import hudson.plugins.s3.MD5;
@@ -14,18 +15,19 @@ import java.io.IOException;
 public class S3DownloadCallable extends S3Callable
 {
     private static final long serialVersionUID = 1L;
-    final private Destination dest;
+    private final Destination dest;
     
-    public S3DownloadCallable(String accessKey, Secret secretKey, boolean useRole, Destination dest, String region)
+    public S3DownloadCallable(String accessKey, Secret secretKey, boolean useRole, Destination dest, String region, ProxyConfiguration proxy)
     {
-        super(accessKey, secretKey, useRole, region);
+        super(accessKey, secretKey, useRole, region, proxy);
         this.dest = dest;
     }
 
-    public FingerprintRecord invoke(File file, VirtualChannel channel) throws IOException, InterruptedException 
+    @Override
+    public FingerprintRecord invoke(File file, VirtualChannel channel) throws IOException, InterruptedException
     {
-        GetObjectRequest req = new GetObjectRequest(dest.bucketName, dest.objectName);
-        Download download = getTransferManager().download(req, file);
+        final GetObjectRequest req = new GetObjectRequest(dest.bucketName, dest.objectName);
+        final Download download = getTransferManager().download(req, file);
 
         download.waitForCompletion();
 
