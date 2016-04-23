a21068edf8d6c0f1cc9f524e1a0a678416011aa6
Alexander <mr.akbashev@gmail.com>
2016-04-23 10:22:09 +0200

    Don't stop uploading if build is not completed

--- src/main/java/hudson/plugins/s3/S3BucketPublisher.java
+++ src/main/java/hudson/plugins/s3/S3BucketPublisher.java
@@ -121,13 +121,11 @@ public final class S3BucketPublisher extends Recorder implements SimpleBuildStep
     public void perform(@Nonnull Run<?, ?> run, @Nonnull FilePath ws, @Nonnull Launcher launcher, @Nonnull TaskListener listener)
             throws InterruptedException {
 
-        if (run.getResult() == null) {
-            log(listener.getLogger(), "Build was not finished correctly. Is not compeleted yet: " + run.isBuilding());
-            run.setResult(Result.UNSTABLE);
-            return;
+        if (run.isBuilding()) {
+            log(listener.getLogger(), "Build is still running");
         }
 
-        final boolean buildFailed = run.getResult().equals(Result.FAILURE);
+        final boolean buildFailed = Result.FAILURE.equals(run.getResult());
 
         final S3Profile profile = getProfile();
         if (profile == null) {
